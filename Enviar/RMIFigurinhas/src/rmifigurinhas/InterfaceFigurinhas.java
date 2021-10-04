1b1ca9e683cdc8c791cf09ce3df8744f26d5397b
Samuel Sena <samuelpedro1999@gmail.com>
2021-10-03 21:15:58 -0300

    Modifiquei nome de projeto

---
+++ Enviar/RMIFigurinhas/src/rmifigurinhas/InterfaceFigurinhas.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rmifigurinhas;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ *
+ * @author grobs
+ */
+public interface InterfaceFigurinhas extends Remote {
+    public Usuario loga_usuario(String nome,String senha) throws RemoteException;
+    public boolean cria_usuario(String nome,String senha) throws RemoteException;
+    public boolean atualiza_usuario(Usuario a) throws RemoteException;
+    public VendaFigurinha get_vendas() throws RemoteException;
+    public boolean atualiza_vendas(VendaFigurinha a) throws RemoteException;
+}
---
+++ Enviar/RMIFigurinhas/src/rmifigurinhas/RMIServer.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rmifigurinhas;
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+/**
+ *
+ * @author grobs
+ */
+public class RMIServer {
+    
+    public static void main(String args[]) 
+    {
+        try {
+            Registry reg = LocateRegistry.createRegistry(1099);
+            System.out.println("Server do Alpokebum ativo!");
+            Naming.rebind("server", new RMIServerImple());
+        }
+        catch (Exception e) {
+            System.out.println("error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
